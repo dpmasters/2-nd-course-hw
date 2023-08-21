1c5fa790e7c8de83aacd24dc3268b95acffbf526
dpmasters <dmpanfilov@mail.ru>
2023-08-21 17:39:58 +0300

    Adedd mobile vers

--- hw 2+1.js
+++ hw 2+1.js
@@ -1,53 +1,53 @@
-let _ = 10;
-alert(_);
-_ = 20;
-alert(_);
+// let _ = 10;
+// alert(_);
+// _ = 20;
+// alert(_);
 
-let firstIphone = 2007; // год выпуска первого IPhone
-alert(firstIphone);
+// let firstIphone = 2007; // год выпуска первого IPhone
+// alert(firstIphone);
 
-let creatorJS = 'Brendan Eich'; // создатель Java Script
-alert(creatorJS);
+// let creatorJS = 'Brendan Eich'; // создатель Java Script
+// alert(creatorJS);
 
-let x = 10;
-let y = 2;
-alert(x + y);
-alert(x - y);
-alert(x * y);
-alert(x / y);
+// let x = 10;
+// let y = 2;
+// alert(x + y);
+// alert(x - y);
+// alert(x * y);
+// alert(x / y);
 
-let $ = 2;
-let result = $**5;
-alert(result);
+// let $ = 2;
+// let result = $ ** 5;
+// alert(result);
 
-let a = 9;
-let b = 2;
-let newResult = a%b;
-alert(newResult);
+// let a = 9;
+// let b = 2;
+// let newResult = a % b;
+// alert(newResult);
 
-let num = 1;
-num += 5;
-num -= 3;
-num *= 7;
-num /= 3;
-num += 1;
-num -= 1;
-alert(num);
+// let num = 1;
+// num += 5;
+// num -= 3;
+// num *= 7;
+// num /= 3;
+// num += 1;
+// num -= 1;
+// alert(num);
 
-let age = prompt('Сколько вам лет?');
-alert(`${age}`);
+// let age = prompt('Сколько вам лет?');
+// alert(`${age}`);
 
-const user = {
-    name: "Dave",
-    age: 37,
-    isAdmin: true,
-}
-user['cityOfResidence'] = "Oremburg";
-user['age'] = 25;
-delete user['cityOfResidence'];
-let info = prompt('Какую информацию хотите узнать о пользователе?');
-alert(user[info]);
+// const user = {
+//     name: "Dave",
+//     age: 37,
+//     isAdmin: true,
+// }
+// user['cityOfResidence'] = "Oremburg";
+// user['age'] = 25;
+// delete user['cityOfResidence'];
+// let info = prompt('Какую информацию хотите узнать о пользователе?');
+// alert(user[info]);
 
-let name = prompt('Ваше имя?');
-let input = name;
-alert(`Привет ${input}!`);
+// let name = prompt('Ваше имя?');
+// let input = name;
+// alert(`Привет ${input}!`);
