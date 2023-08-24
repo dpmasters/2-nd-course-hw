95e6df0b1adf4aac5055cdb4a7ec71a7050d198f
dpmasters <dmpanfilov@mail.ru>
2023-08-24 18:12:01 +0300

    Corrected mobile,scss

---
+++ hw-5.js
@@ -0,0 +1,27 @@
+//#1
+// const sim = (a, b) => {
+//     if (a < b) {
+//         return a;
+//     } else {
+//         return b;
+//     }
+// }
+
+// console.log(sim(8, 4));
+
+
+//#2
+// let chekNumber = Number(prompt('Пожалуйста, введите любое число'));
+// if (chekNumber % 2 === 0) {
+//     alert('Число четное');
+// } else {
+//     alert('Число не четное')
+// }
+
+const chekNumber = Number(prompt('Пожалуйста, введите любое число'));
+if (chekNumber % 2 === 0) {
+    return 'Число четное';
+} else {
+    return 'Число не четное';
+}
+
